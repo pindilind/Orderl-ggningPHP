fed713f46cbdb2b196dcc347d12a1ea3bd205075
pindilind <linda.gustafsson@outlook.com>
2021-01-27 16:59:28 +0100

    produkter, knappar är på sidan

--- server/productReciever.php
+++ server/productReciever.php
@@ -1,36 +1,30 @@
-<?php 
+<?php
 require("./classes.php");
 
 
 
 /* array med producter */
-    $products = [
-    new Product("Iphone 11", 100000, "200g"),
+$products = [
+    new Product("Iphone 11", 10000, "200g"),
     new Product("Huawei P20", 2000, "50g"),
-    new Product("Sony S5", 7300, "150g")
+    new Product("Sony S5", 7300, "150g"),
+    new Product("Nokia 3310", 2000, "500g"),
+    new Product("Iphone 12", 13999, "150g")   
 ];
-
-/* $productToOrder = [];
-
-foreach ($products as $product) {
-    array_push($productToOrder, 
-    new Product($product["name"], $product["price"], $product["weight"]));
-}  */
-
 
 
 
 try {
     session_start();
 
-   //Kollar om en request har gjorts
-   if (isset($_SERVER["REQUEST_METHOD"])) {
+    //Kollar om en request har gjorts
+    if (isset($_SERVER["REQUEST_METHOD"])) {
 
-    if ($_SERVER["REQUEST_METHOD"] === "GET") {
-        //REQUEST_METHOD is GET
-       
-        
+        if ($_SERVER["REQUEST_METHOD"] === "GET") {
+            //REQUEST_METHOD is GET
+
             echo json_encode($products);
+
             exit;
         } else {
             //skickar feedback att inga producter är sparat
@@ -41,14 +35,11 @@ try {
         throw new Exception("Not a valid request...", 405);
     }
 } catch (Exception $error) {
-echo json_encode(
-    array(
-        "Message" => $error->getMessage(),
-        "Status" => $error->getCode()
-    )
-);
-exit;
+    echo json_encode(
+        array(
+            "Message" => $error->getMessage(),
+            "Status" => $error->getCode()
+        )
+    );
+    exit;
 }
-
-
-?>
