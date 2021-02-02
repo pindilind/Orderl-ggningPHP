12a3f21082504afed70eb236be76ed8b79a7f3bf
pindilind <linda.gustafsson@outlook.com>
2021-02-02 13:45:21 +0100

    kan spara produkter i localstorage

--- logic.js
+++ logic.js
@@ -4,24 +4,24 @@ document.getElementById("submitBtn").addEventListener("click", submitOrder)
 
 function initsite() {
     seeProductsOnPage()
-   
+
 }
 
-/* function för knappen "submit" */
+// function för knappen "submit"
 async function submitOrder() {
     console.log("Hejhej")
-} 
+}
 
-/* function för att skriva ut produkter på sidan */
+// function för att skriva ut produkter på sidan
 async function seeProductsOnPage() {
     const productInput = document.getElementById("seeProduct")
     const productList = await request("./server/productReciever.php", "GET")
     console.log(productList)
 
 
-    
+
     // Nytt sätt att skriva en for-loop på (skriver ut på sidan)
-        productList.forEach((product) => {
+    productList.forEach((product) => {
         let productContainer = document.createElement("div")
 
         let nameText = document.createElement("h3")
@@ -32,47 +32,16 @@ async function seeProductsOnPage() {
 
         let weightText = document.createElement("h5")
         weightText.innerText = product.weight
-        
+
         let cartButton = document.createElement("button")
         let buttonText = document.createElement("h3")
-            buttonText.innerText = "Lägg till i kundvagnen"
-            cartButton.data = product
+        buttonText.innerText = "Lägg till i kundvagnen"
+        cartButton.data = product
 
-        cartButton.onclick = function() {
-            console.log(this.data)
-            addToCart()
-        }
+        //knapp för att lägga till i localstorage
+        cartButton.addEventListener("click", addToCart) 
 
-        function addToCart() {
-        
-        
-            let productToAdd = this.data
-            let cart = JSON.parse(localStorage.getItem("cart"))
 
-        if(!cart) {
-            cart = []
-        } else {
-            cart = JSON.parse(cart)
-        }
-
-       let foundIndex =  cart.findIndex((cartItem) => {
-            return cartItem.products.id == productToAdd.id
-        }) 
-        console.log(foundIndex)
-
-        if(foundIndex != -1) {
-            cart[foundIndex].quantity++
-        } else {
-            cart.push({
-                product: productToAdd,
-                quantity: 1
-            })
-        }
-
-    
-
-    }
-       
         //Appendar till sidan
         productContainer.append(nameText, priceText, weightText, cartButton)
         cartButton.appendChild(buttonText)
@@ -82,19 +51,53 @@ async function seeProductsOnPage() {
         //Classlists för styling
         productContainer.classList = "productContainer"
         cartButton.classList = "cartButton"
-        
+
     })
 
+    //funktion för att lägga till i localstorage
+    function addToCart() {
+
+        let productToAdd = this.data
+        console.log(productToAdd)
+        let cart = localStorage.getItem("cart")
+        console.log(cart)
+       if(!cart) {
+            cart = []
+        }else {
+            cart = JSON.parse(cart)
+        }
+
+        let foundIndex = cart.findIndex((cartItem) => {
+            return cartItem.product.id == productToAdd.id
+        })
+    
+
+        if(foundIndex != -1) {
+            cart[foundIndex].quantity++
+        
+        } else {
+        cart.push({
+            product: productToAdd,
+            quantity: 1
+        })
+    }
+        
+     localStorage.setItem("cart", JSON.stringify(cart))
+    
+    }
+
+
     /* Foreachloop  */
-    productList.forEach((product) => {
+    /* productList.forEach((product) => {
         // Printa produktkort
         console.log(product)
     })
 
-    
+
 }
+ */
 
-
+ //request funktion 
 async function request(path, method, body) {
     try {
         const response = await fetch(path, {
@@ -109,5 +112,4 @@ async function request(path, method, body) {
 }
 
 
- /* formatet för 1 produkt i carten */
-       
+    }
